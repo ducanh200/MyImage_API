184b932b3cf2a502eb343606a13ddfd6eca232be
ducanh2004 <ducanh2004@users.noreply.github.com>
2024-01-13 13:22:24 +0700

    push~end

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -468,5 +468,222 @@ namespace MyImage_API.Controllers
             }
         }
 
+        [HttpGet]
+        [Route("today-orders-amount")]
+        public IActionResult TodayOrdersAmount()
+        {
+            try
+            {
+                DateTime today = DateTime.Today;
+                DateTime tomorrow = today.AddDays(1);
+
+                int totalAmount = _context.Orders
+                    .Where(o => o.CreatedAt >= today && o.CreatedAt < tomorrow && o.Status != 0)
+                    .Sum(o => o.TotalAmount);
+
+                return Ok(new { total_amount = totalAmount });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error retrieving total amount of orders created today with status not equal to 0: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("yesterday-orders-amount")]
+        public IActionResult YesterdayOrdersAmount()
+        {
+            try
+            {
+                DateTime yesterday = DateTime.Today.AddDays(-1);
+                DateTime today = DateTime.Today;
+
+                int totalAmount = _context.Orders
+                    .Where(o => o.CreatedAt >= yesterday && o.CreatedAt < today && o.Status != 0)
+                    .Sum(o => o.TotalAmount);
+
+                return Ok(new { total_amount = totalAmount });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error retrieving total amount of orders created yesterday with status not equal to 0: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("2-days-ago-orders-amount")]
+        public IActionResult TwoDaysAgoOrdersAmount()
+        {
+            try
+            {
+                DateTime twoDaysAgo = DateTime.Today.AddDays(-2);
+                DateTime yesterday = DateTime.Today.AddDays(-1);
+
+                int totalAmount = _context.Orders
+                    .Where(o => o.CreatedAt >= twoDaysAgo && o.CreatedAt < yesterday && o.Status != 0)
+                    .Sum(o => o.TotalAmount);
+
+                return Ok(new { total_amount = totalAmount });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error retrieving total amount of orders created two days ago with status not equal to 0: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("3-days-ago-orders-amount")]
+        public IActionResult ThreeDaysAgoOrdersAmount()
+        {
+            try
+            {
+                DateTime DaysAgo = DateTime.Today.AddDays(-3);
+                DateTime timeday = DateTime.Today.AddDays(-2);
+
+                int totalAmount = _context.Orders
+                    .Where(o => o.CreatedAt >= DaysAgo && o.CreatedAt < timeday && o.Status != 0)
+                    .Sum(o => o.TotalAmount);
+
+                return Ok(new { total_amount = totalAmount });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error retrieving total amount of orders created days ago with status not equal to 0: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("4-days-ago-orders-amount")]
+        public IActionResult FourDaysAgoOrdersAmount()
+        {
+            try
+            {
+                DateTime DaysAgo = DateTime.Today.AddDays(-4);
+                DateTime timeday = DateTime.Today.AddDays(-3);
+
+                int totalAmount = _context.Orders
+                    .Where(o => o.CreatedAt >= DaysAgo && o.CreatedAt < timeday && o.Status != 0)
+                    .Sum(o => o.TotalAmount);
+
+                return Ok(new { total_amount = totalAmount });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error retrieving total amount of orders created days ago with status not equal to 0: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("5-days-ago-orders-amount")]
+        public IActionResult FiveDaysAgoOrdersAmount()
+        {
+            try
+            {
+                DateTime DaysAgo = DateTime.Today.AddDays(-5);
+                DateTime timeday = DateTime.Today.AddDays(-4);
+
+                int totalAmount = _context.Orders
+                    .Where(o => o.CreatedAt >= DaysAgo && o.CreatedAt < timeday && o.Status != 0)
+                    .Sum(o => o.TotalAmount);
+
+                return Ok(new { total_amount = totalAmount });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error retrieving total amount of orders created days ago with status not equal to 0: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("6-days-ago-orders-amount")]
+        public IActionResult SixDaysAgoOrdersAmount()
+        {
+            try
+            {
+                DateTime DaysAgo = DateTime.Today.AddDays(-6);
+                DateTime timeday = DateTime.Today.AddDays(-7);
+
+                int totalAmount = _context.Orders
+                    .Where(o => o.CreatedAt >= DaysAgo && o.CreatedAt < timeday && o.Status != 0)
+                    .Sum(o => o.TotalAmount);
+
+                return Ok(new { total_amount = totalAmount });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error retrieving total amount of orders created days ago with status not equal to 0: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("month-orders-amount/{month}")]
+        public IActionResult MonthOrdersAmount(int month)
+        {
+            try
+            {
+                DateTime startOfMonth = new DateTime(DateTime.Now.Year, month, 1);
+                DateTime endOfMonth = startOfMonth.AddMonths(1);
+
+                int totalAmount = _context.Orders
+                    .Where(o => o.CreatedAt >= startOfMonth && o.CreatedAt < endOfMonth && o.Status != 0)
+                    .Sum(o => o.TotalAmount);
+
+                return Ok(new { total_amount = totalAmount });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error retrieving total amount of orders created in month {month} with status not equal to 0: {ex.Message}");
+            }
+        }
+
+        // Tạo 12 hàm tượng trưng cho 12 tháng
+        [HttpGet]
+        [Route("month1-orders-amount")]
+        public IActionResult Month1OrdersAmount() => MonthOrdersAmount(1);
+
+        [HttpGet]
+        [Route("month2-orders-amount")]
+        public IActionResult Month2OrdersAmount() => MonthOrdersAmount(2);
+
+        [HttpGet]
+        [Route("month3-orders-amount")]
+        public IActionResult Month3OrdersAmount() => MonthOrdersAmount(3);
+
+        [HttpGet]
+        [Route("month4-orders-amount")]
+        public IActionResult Month4OrdersAmount() => MonthOrdersAmount(4);
+
+        [HttpGet]
+        [Route("month5-orders-amount")]
+        public IActionResult Month5OrdersAmount() => MonthOrdersAmount(5);
+
+        [HttpGet]
+        [Route("month6-orders-amount")]
+        public IActionResult Month6OrdersAmount() => MonthOrdersAmount(6);
+
+        [HttpGet]
+        [Route("month7-orders-amount")]
+        public IActionResult Month7OrdersAmount() => MonthOrdersAmount(7);
+
+        [HttpGet]
+        [Route("month8-orders-amount")]
+        public IActionResult Month8OrdersAmount() => MonthOrdersAmount(8);
+
+        [HttpGet]
+        [Route("month9-orders-amount")]
+        public IActionResult Month9OrdersAmount() => MonthOrdersAmount(9);
+
+        [HttpGet]
+        [Route("month10-orders-amount")]
+        public IActionResult Month10OrdersAmount() => MonthOrdersAmount(10);
+
+        [HttpGet]
+        [Route("month11-orders-amount")]
+        public IActionResult Month11OrdersAmount() => MonthOrdersAmount(11);
+
+        [HttpGet]
+        [Route("month12-orders-amount")]
+        public IActionResult Month12OrdersAmount() => MonthOrdersAmount(12);
+
     }
 }
